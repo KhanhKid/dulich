4a218888016cf1d9d478df6da53d15fa39206052
KhanhKid <khanhvo@vinaresearch.net>
2015-04-22 19:35:00 +0700

    lien he

--- system/application/views/a_lienhe.php
+++ system/application/views/a_lienhe.php
@@ -3,33 +3,46 @@
 			    <div id="left-news">
 	<div class="pathway-news">
       			<div itemscope=""><a href="/" itemprop="url" class="homepage"><span itemprop="title">Trang chủ</span></a></div>
-      			
-     			<div itemscope=""  class="no-bg"><a href="/a/lienhe" itemprop="url" class="homepage"><span itemprop="title">Liện hệ</span></a></div>
-         
-      
-	</div>
-			
-	
-	<div class="sub-left">	
 
-		<p>Đang cập nhật.... </p>
-		
+     			<div itemscope=""  class="no-bg"><a href="/a/lienhe" itemprop="url" class="homepage"><span itemprop="title">Liện hệ</span></a></div>
+
+
 	</div>
-	
+
+
+	<div class="sub-left">
+
+		<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=C%C3%94NG%20TY%20TNHH%20TH%C6%AF%C6%A0NG%20M%E1%BA%A0I%20V%C3%80%20D%E1%BB%8ACH%20V%E1%BB%A4%20DU%20L%E1%BB%8ACH%20SUNRISE&key=AIzaSyDgs91_EZvfzEq7ij99hP3Z4MWAzI3ldBs"></iframe>
+	 <table>
+    <tr>
+      <td>
+        <ul class="items-address">
+          <li class="name">CÔNG TY TNHH THƯƠNG MẠI VÀ DỊCH VỤ DU LỊCH SUNRISE</li>
+          <li>18/4B Đường Đông Hưng Thuận 2, Phường Đông Hưng Thuận, Quận 12, TP.HCM, Việt Nam</li>
+          <li><strong>ĐT:</strong> (08) 37152530 - <strong> Fax: </strong>(08) 37152530</li>
+          <li><strong>Email:</strong> sunrisetsttravel@gmail.com</li>
+          <li>Hotline: 0919 095 778 (Mr.Tâm) / 0916 065 778 (Mss.Trang)</li>
+        </ul>
+      </td>
+
+    </tr>
+   </table>
+  </div>
+
 	</div>
 <div id="right-news">
-        
-            
-        
+
+
+
     <div class="group-photo">
-        
+
     </div>
-    
+
     <div class="ads-news-r">
-    
+
     </div>
-    
-    
+
+
     <div class="box-online">
         <div id="fb-root"></div>
             <script>(function(d, s, id) {
@@ -40,10 +53,9 @@
               fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));</script>
         <div class="fb-page" data-href="https://www.facebook.com/pages/Sunrise-TST-Travel/1569215376630971?__mref=message_bubble" data-width="300" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pages/Sunrise-TST-Travel/1569215376630971?__mref=message_bubble"><a href="https://www.facebook.com/pages/Sunrise-TST-Travel/1569215376630971?__mref=message_bubble">Sunrise TST Travel</a></blockquote></div></div>
-        
-    </div>    
-</div>					
+
+    </div>
 </div>
-	
-		
-		
+</div>
+
+
